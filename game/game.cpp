835eaf4900e37f7245f68f92e088952215ebb87f
Mikhail <eremin.ms92@gmaill.com>
2025-03-05 16:22:56 +0200

    Add Dear ImGui integration to the project
    Integrated Dear ImGui library with SDL3 and SDL Renderer 3 backends. Configured build system and updated the application to initialize, render, and dispose ImGui properly. Included support for Dear ImGui docking branch as a submodule.

--- game/game.cpp
+++ game/game.cpp
@@ -1,6 +1,9 @@
 #include <stdexcept>
 
 #include "SDL3/SDL.h"
+#include "imgui.h"
+#include "backends/imgui_impl_sdl3.h"
+#include "backends/imgui_impl_sdlrenderer3.h"
 
 struct Application
 {
@@ -34,11 +37,25 @@ Application init()
         throw std::runtime_error(SDL_GetError());
     }
 
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+    ImGui_ImplSDL3_InitForSDLRenderer(app.window, app.renderer);
+    ImGui_ImplSDLRenderer3_Init(app.renderer);
+
     return app;
 }
 
-void dispose(Application& app)
+void dispose(const Application& app)
 {
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
     SDL_DestroyRenderer(app.renderer);
     SDL_DestroyWindow(app.window);
     SDL_Quit();
@@ -59,10 +76,14 @@ int main(int argc, char* argv[])
     }
 
     bool shouldQuit = false;
-    while (!shouldQuit) {
+    while (!shouldQuit)
+    {
         SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT || (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_ESCAPE)) {
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL3_ProcessEvent(&event);
+            if (event.type == SDL_EVENT_QUIT || (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_ESCAPE))
+            {
                 shouldQuit = true;
             }
         }
@@ -70,6 +91,15 @@ int main(int argc, char* argv[])
         // gray background
         SDL_SetRenderDrawColor(app.renderer, 92, 105, 108, SDL_ALPHA_OPAQUE);
         SDL_RenderClear(app.renderer);
+
+        ImGui_ImplSDL3_NewFrame();
+        ImGui_ImplSDLRenderer3_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Render();
+        ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), app.renderer);
 
         SDL_RenderPresent(app.renderer);
     }
