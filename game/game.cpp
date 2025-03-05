f63c59d1eb478e5ae3bb2473a85322d4843230ec
Mikhail <eremin.ms92@gmaill.com>
2025-03-05 15:42:51 +0200

    Add SDL integration and initialize basic rendering loop
    Integrated SDL as a submodule and updated the project to use its library. Added initialization logic, created a basic event loop, and implemented a gray background rendering. Updated configurations to support SDL, removed unused Win32 settings, and upgraded the language standard to C++23.

--- game/game.cpp
+++ game/game.cpp
@@ -1,6 +1,79 @@
+#include <stdexcept>
+
+#include "SDL3/SDL.h"
+
+struct Application
+{
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+};
+
+Application init()
+{
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        throw std::runtime_error(SDL_GetError());
+    }
+
+    Application app;
+    bool isWindowCreated = SDL_CreateWindowAndRenderer(
+        "Game",
+        1280,
+        720,
+        0,
+        &app.window,
+        &app.renderer
+    );
+    if (!isWindowCreated)
+    {
+        throw std::runtime_error(SDL_GetError());
+    }
+
+    if (!SDL_SetRenderVSync(app.renderer, 1))
+    {
+        throw std::runtime_error(SDL_GetError());
+    }
+
+    return app;
+}
+
+void dispose(Application& app)
+{
+    SDL_DestroyRenderer(app.renderer);
+    SDL_DestroyWindow(app.window);
+    SDL_Quit();
+}
 
 int main(int argc, char* argv[])
 {
-    
+    Application app;
+    try
+    {
+        app = init();
+    }
+    catch (const std::exception& e)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "%s", e.what());
+        dispose(app);
+        return 1;
+    }
+
+    bool shouldQuit = false;
+    while (!shouldQuit) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT || (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_ESCAPE)) {
+                shouldQuit = true;
+            }
+        }
+
+        // gray background
+        SDL_SetRenderDrawColor(app.renderer, 92, 105, 108, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(app.renderer);
+
+        SDL_RenderPresent(app.renderer);
+    }
+
+    dispose(app);
     return 0;
 }
